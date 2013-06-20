14a71b70fe3c063d519cd79af11d9873920e96d8
Knud Möller <dunken69@gmail.com>
2013-06-20 12:30:14 +0200

    logging level changed

--- ckanext/stromnetzberlin/ckanharvester.py
+++ ckanext/stromnetzberlin/ckanharvester.py
@@ -53,7 +53,7 @@ class StromnetzBerlinCKANHarvester(GroupCKANHarvester):
                 'compatibility problems.'}
 
     def amend_package(self, package):
-        log.warning("Ammending package '{name}'".format(name=package["name"]))
+        log.debug("Amending package '{name}'".format(name=package["name"]))
         package['groups'] = ['verentsorgung']
         
         # turn the date arrays into individual extras entries
